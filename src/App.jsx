6371b7ac61e133ca350a04a9e2933aebdc45c8a6
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-08-20 18:05:23 -0300

    base subHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
 import ScrollToTopButton from './components/ScrollToTopButton';
 import Slide from './components/Slide';
@@ -6,14 +8,25 @@ import BrowseCategories from './components/BrowseCategories';
 import Footer from './components/Footer';
 
 export default function App() {
+  const [page, setPage] = useState('home');
+
   return (
-    <div className="flex-1 w-full max-w-screen-xl mx-auto">
-      <Header />
-      <Slide />
-      <div className="w-full px-4 m-0 p-0">
-        <ResumeCatalog />
-        <BrowseCategories />
-      </div>
+    <div className="flex-1 w-full max-w-screen-xl mx-auto border border-[#ccc]">
+      <Header page={page} setPage={setPage} />
+      {page === 'home' ? (
+        <>
+          <Slide />
+          <div className="w-full px-4 m-0 p-0">
+            <ResumeCatalog />
+            <BrowseCategories />
+          </div>
+        </>
+      ) : null}
+
+      {page === 'catalog' ? <ResumeCatalog /> : null}
+
+      {page === 'categories' ? <BrowseCategories /> : null}
+
       <Footer />
       <ScrollToTopButton />
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,54 @@
-export default function Header() {
+export default function Header({ page, setPage }) {
   return (
     <>
-      <div className="h-[72px] w-full max-w-screen-xl mx-auto flex items-center justify-between px-4  bg-gray-100 fixed top-0 z-10 ">
-        <div className="flex items-center gap-4">
-          <img
-            src={`${import.meta.env.BASE_URL}/logo.jpg`}
-            alt="logo"
-            className="h-[50px] rounded-lg"
-          />
-          <h1 className="md:block hidden">Velas e Aroma</h1>
-        </div>
+      <div className="w-full max-w-screen-xl mx-auto bg-gray-100 fixed top-0 z-10 ">
+        <div className="flex items-center justify-between px-4 ">
+          <div className="flex items-center gap-4">
+            <img
+              src={`${import.meta.env.BASE_URL}/logo.jpg`}
+              alt="logo"
+              className="h-[50px] rounded-lg"
+            />
+            <h1 className="md:block hidden">Velas e Aroma</h1>
+          </div>
 
-        <div className="w-[60%] relative">
-          <input
-            type="text"
-            placeholder="Search"
-            className="border-b border-b-gray-900 p-1 bg-gray-100 w-full focus:outline-none rounded-md"
-          />
-          <button className="ml-2 w-[40px] absolute right-0">
-            <i className="fas fa-search"></i>
-          </button>
+          <div className="w-[60%] relative">
+            <input
+              type="text"
+              placeholder="Search"
+              className="border-b border-b-gray-900 p-1 bg-gray-100 w-full focus:outline-none rounded-md"
+            />
+            <button className="ml-2 w-[40px] absolute right-0">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+          <a
+            href="https://github.com/vela-e-aroma/vela-e-aroma"
+            className="flex items-center"
+          >
+            <span className="md:block hidden">Atendimento</span>
+            <i className="fas fa-phone-square ms-3"></i>
+          </a>
         </div>
-        <a
-          href="https://github.com/vela-e-aroma/vela-e-aroma"
-          className="flex items-center"
-        >
-          <span className="md:block hidden">Atendimento</span>
-          <i className="fas fa-phone-square ms-3"></i>
-        </a>
-      
+        <div className="w-full flex items-center justify-between p-1 mt-1 bg-[#e3e6e6] px-4">
+          {[
+            { label: 'Home', pageName: 'home' },
+            { label: 'Catálogo', pageName: 'catalog' },
+            { label: 'Sobre as velas', pageName: 'about' },
+            { label: 'Monte sua vela', pageName: 'order' },
+          ].map(({ label, pageName }) => (
+            <p
+              key={pageName}
+              onClick={() => setPage(pageName)}
+              className={`${
+                page === pageName ? 'font-bold' : ''
+              } cursor-pointer`}
+            >
+              {label}
+            </p>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
